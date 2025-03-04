dc22c8a74cd92b86ff5eac3b1fe98735f3699d59
Aqib2110 <muhammadaqib2110@gmail.com>
2025-03-04 21:32:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ origin:["https://todo-92cn.vercel.app"],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true 
 }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://todo-nbdm.vercel.app');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.options('*', cors());
 const users = [];
 app.use(express.urlencoded({extended:true}))
