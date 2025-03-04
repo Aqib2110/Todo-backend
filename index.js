ee7d7d165f8ff6ac387cf7e52fae2c12e9cafab3
Aqib2110 <muhammadaqib2110@gmail.com>
2025-03-04 21:45:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -25,11 +25,16 @@ origin:["https://todo-92cn.vercel.app"],
     credentials: true 
 }));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://todo-nbdm.vercel.app');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+    res.setHeader('Access-Control-Allow-Origin', 'https://todo-nbdm.vercel.app');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(204); // Preflight request response
+    } else {
+        next();
+    }
 });
 
 app.options('*', cors());
